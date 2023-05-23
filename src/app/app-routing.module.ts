31072fc137e75991cc0fb06e6715f4cac517e4a1
Sebastian Szuch <sjszuch@gmail.com>
2023-05-23 13:17:28 -0400

    Updated start screen, began fixing gif loops

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartscreenComponent } from './components/startscreen/startscreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: StartscreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
