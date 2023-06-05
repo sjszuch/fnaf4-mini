f55db3b8f97a89fbc0f4d8181d5aba3a5d814d0c
Sebastian Szuch <sjszuch@gmail.com>
2023-06-05 11:44:56 -0400

    Updated pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StartscreenComponent } from './components/startscreen/startscreen.component';
 
 const routes: Routes = [
-  { path: '/', component: StartscreenComponent}
+
 ];
 
 @NgModule({
